23c737f1da2b71f7c28892332c92063b70153615
aastha <aamujawdiya@bestpeersgmail.com>
2023-04-07 10:48:08 +0530

    initial commit

--- literals.rb
+++ literals.rb
@@ -1,1 +1,22 @@
-puts "Hello"
+#integer literals
+puts 354
+#float literal
+puts 3.14
+#string literal
+puts "hello"
+#hash literal
+hash={:ayu=>1,:adi=>2,:aas=>3}
+puts hash[:aas]
+#array literal
+array=[1,2,3,4,5]
+puts array[0]
+#string interpolation
+a='two'
+puts "hello,i m aastha,#{a}"
+b='four'
+puts 'hello,aastha #{a}'
+#boolean literal
+puts 10==10
+c=20
+d=20
+puts c==d

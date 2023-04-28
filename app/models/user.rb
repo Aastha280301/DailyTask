a0e43aa609dc9fe50c14d10ee23d798b564d5d70
aastha <aamujawdiya@bestpeersgmail.com>
2023-04-28 16:46:23 +0530

    28april

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :phoneNumber, presence: true
-  validates :user_details, presence: true
+  validates :name, :email, :phoneNumber,:user_details ,presence: true
+  
 end
